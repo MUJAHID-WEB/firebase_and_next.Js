be246f3c40be78636ce7aaf9bdf6fa85d482fbb6
MUJAHID-WEB <mujahid30390@gmail.com>
2024-03-28 12:31:25 +0600

    login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,35 @@
+'use client'
+
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import {auth} from '../firebaseConfig'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import Link from 'next/link'
-import React from 'react'
 
 
 function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const router = useRouter()
+
+    const login = async () =>{
+        if(email === '' || password === ''){
+            return alert('Please fill all fields')
+        }
+        try{
+            const user = await signInWithEmailAndPassword(auth, email, password)
+
+            alert('Login Successfully')
+            router.push('/');
+
+            setEmail('')
+            setPassword('')
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+
    
     return (
         <div className=' flex justify-center items-center h-screen'>
@@ -13,6 +40,8 @@ function Login() {
                 <div>
                     <input type="email"
                         name='email'
+                        value={email}
+                        onChange={(e)=>setEmail(e.target.value)}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
                     />
@@ -20,12 +49,15 @@ function Login() {
                 <div>
                     <input
                         type="password"
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Password'
                     />
                 </div>
                 <div className=' flex justify-center mb-3'>
                     <button
+                    onClick={login}
                         className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
                         Login
                     </button>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,56 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
 
-function Navbar() {
+import Link from "next/link";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+import { auth } from "../app/firebaseConfig";
+
+function Navbar({ user }) {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await auth.signOut();
+      router.push("login");
+    } catch (error) {
+      console.error(" error logging out: ", error);
+    }
+  };
+
   return (
-    <div className='main lg:flex md:flex flex-wrap justify-between items-center 
-     px-4 bg-[#2a056f] py-4 shadow-md'>
-            <div className="left">
-                <div className="logo font-bold text-2xl text-white text-center"></div>
-            </div>
-            <div className="right">
-                <ul className='flex space-x-4 text-white justify-center items-center'>
-                    <Link href={'/'}>
-                        <li className='cursor-pointer'>Home</li>
-                    </Link>
-                    <Link href={'/login'}>
-                        <li className='cursor-pointer'>Login</li>
-                    </Link>
-                    <Link href={'/signup'}>
-                        <li className='cursor-pointer'>Signup</li>
-                    </Link>
-                </ul>
-            </div>
-        </div>
-  )
+    <div
+      className="main lg:flex md:flex flex-wrap justify-between items-center 
+     px-4 bg-[#2a056f] py-4 shadow-md"
+    >
+      <div className="left">
+        <div className="logo font-bold text-2xl text-white text-center"></div>
+      </div>
+      <div className="right">
+        {user ? (
+          <div className="flex space-x-4 text-yellow-300 justify-center items-center">
+            <p>Welcome, {user.email}</p>
+            <button onClick={logout} className="cursor-pointer">
+              Logout
+            </button>
+          </div>
+        ) : (
+          <ul className="flex space-x-4 text-white justify-center items-center">
+            <Link href={"/"}>
+              <li className="cursor-pointer">Home</li>
+            </Link>
+
+            <Link href={"/login"}>
+              <li className="cursor-pointer">Login</li>
+            </Link>
+            <Link href={"/signup"}>
+              <li className="cursor-pointer">Signup</li>
+            </Link>
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
